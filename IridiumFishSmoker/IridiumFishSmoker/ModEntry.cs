afb4b552f81aef479cdba8d0f873e425c89434c8
Patrick Critz <xeru98@gmail.com>
2024-06-02 10:44:37 -0400

    Base for iridium smoker

---
+++ IridiumFishSmoker/IridiumFishSmoker/ModEntry.cs
@@ -0,0 +1,11 @@
+﻿using StardewModdingAPI;
+
+namespace IridiumFishSmoker.IridiumFishSmoker;
+
+public class ModEntry : Mod
+{
+    public override void Entry(IModHelper helper)
+    {
+        
+    }
+}
